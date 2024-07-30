9f042377b9b06cc5ccf594a33c446b48928c92f1
biniyamNegasa <104790161+biniyamNegasa@users.noreply.github.com>
2024-07-30 14:35:39 +0300

    Make the program work in terminal

--- dist/todo.js
+++ dist/todo.js
@@ -1,4 +1,33 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const readline = __importStar(require("readline"));
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 ;
 class TodoItem {
     constructor() {
@@ -33,36 +62,43 @@ class TodoItem {
 }
 ;
 const todo = new TodoItem();
-let haveConversation = true;
-while (haveConversation) {
-    console.log(`Welcome to your todo app!\n`);
+function conversation() {
+    console.log(`\n\nWelcome to your todo app!\n`);
     console.log(`Choose what you want to do: `);
     console.log(`1 to display your todo lists`);
     console.log(`2 to add new todo`);
     console.log(`3 to remove a todo`);
     console.log(`4 to end this conversation`);
-    let userInput = prompt("Insert your choice here");
-    if (userInput == null || +userInput < 1 || +userInput > 4) {
-        console.log("You have to choose one of the numbers from the given alternatives.");
-    }
-    else {
-        switch (+userInput) {
-            case 1:
-                todo.display();
-                break;
-            case 2:
-                let text = prompt("Please provide your todo.");
-                if (text != null)
-                    todo.add(text);
-                break;
-            case 3:
-                let index = prompt("Please provide the index of the todo you want to remove.");
-                if (index != null)
-                    todo.remove(+index - 1);
-                break;
-            case 4:
-                haveConversation = false;
-                break;
+    rl.question("Insert your choice here: ", (userInput) => {
+        if (userInput == null || Number.isNaN(+userInput) || +userInput < 1 || +userInput > 4) {
+            console.log("You have to choose one of the numbers from the given alternatives.");
+            conversation();
         }
-    }
+        else {
+            switch (+userInput) {
+                case 1:
+                    todo.display();
+                    conversation();
+                    break;
+                case 2:
+                    rl.question("Please provide your todo: ", (text) => {
+                        if (text != null)
+                            todo.add(text);
+                        conversation();
+                    });
+                    break;
+                case 3:
+                    rl.question("Please provide the index of the todo you want to remove: ", (index) => {
+                        if (index != null)
+                            todo.remove(+index - 1);
+                        conversation();
+                    });
+                    break;
+                case 4:
+                    rl.close();
+                    break;
+            }
+        }
+    });
 }
+conversation();
--- src/todo.ts
+++ src/todo.ts
@@ -1,3 +1,11 @@
+import * as readline from 'readline';
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+
 interface TodoItemInterface {
     todoList: string[];
     size: number;
@@ -44,41 +52,49 @@ class TodoItem implements TodoItemInterface {
 
 const todo: TodoItem = new TodoItem();
 
-let haveConversation: boolean = true;
 
-while (haveConversation){
-    console.log(`Welcome to your todo app!\n`);
+function conversation(): void {
+    
+    console.log(`\n\nWelcome to your todo app!\n`);
     console.log(`Choose what you want to do: `);
     console.log(`1 to display your todo lists`);
     console.log(`2 to add new todo`);
     console.log(`3 to remove a todo`);
     console.log(`4 to end this conversation`);
-
-    let userInput = prompt("Insert your choice here");
     
-    if ( userInput == null || +userInput < 1 || +userInput > 4){
-        console.log("You have to choose one of the numbers from the given alternatives.");
-    }
-    else{
-        switch(+userInput){
-            case 1:
-                todo.display();
-                break;
-            case 2:
-                let text: string | null = prompt("Please provide your todo.");
-                if (text != null)
-                    todo.add(text);
-                break;
-            case 3:
-                let index = prompt("Please provide the index of the todo you want to remove.");
-                if (index != null)
-                    todo.remove(+index - 1);
-                break;
-            case 4:
-                haveConversation = false;
-                break;
-            
+    
+    rl.question("Insert your choice here: ", (userInput) => {
+        if ( userInput == null || Number.isNaN(+userInput) || +userInput < 1 || +userInput > 4){
+            console.log("You have to choose one of the numbers from the given alternatives.");
+            conversation();
         }
-    }
-
+        else{
+            switch(+userInput){
+                case 1:
+                    todo.display();
+                    conversation();
+                    break;
+                case 2:
+                    rl.question("Please provide your todo: ", (text) => {
+                        if (text != null)
+                            todo.add(text);
+                        conversation();
+                    });
+                    break;
+                case 3:
+                    rl.question("Please provide the index of the todo you want to remove: ", (index) => {
+                        if (index != null)
+                            todo.remove(+index - 1);
+                        conversation();
+                    });
+                    break;
+                case 4:
+                    rl.close();
+                    break;
+                
+            }
+        }
+    });
 }
+
+conversation();
