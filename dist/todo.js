432158820b67ce767f6ad802319348ec6d61a147
biniyamNegasa <104790161+biniyamNegasa@users.noreply.github.com>
2024-07-30 11:17:30 +0300

    Implement the logic

--- dist/todo.js
+++ dist/todo.js
@@ -1,71 +1,57 @@
-interface TodoItemInterface {
-    todoList: string[];
-    size: number;
-    add(text: string): void;
-    remove(index: number): void;
-    display(): void;
-};
-
-class TodoItem implements TodoItemInterface {
-    todoList: string[] = [];
-    size: number = 0;
-
-    constructor(){};
-
-    add(text: string): void {
+"use strict";
+;
+class TodoItem {
+    constructor() {
+        this.todoList = [];
+        this.size = 0;
+    }
+    ;
+    add(text) {
         text = text.trim();
-
-        if (text){
+        if (text) {
             this.todoList.push(text);
             this.size += 1;
         }
     }
-
-    remove(index: number): void {
-
+    remove(index) {
         if (typeof index == 'number' && index < this.size && index >= 0) {
             this.todoList.splice(index, 1);
-            this.size -= 1
+            this.size -= 1;
         }
     }
-
-    display(): void {
-        if (this.size > 0){
+    display() {
+        if (this.size > 0) {
             console.log(`Here are your todo lists.\n`);
-            for (let i = 0; i < this.size; i++){
-                console.log(i+1, this.todoList[i]);
+            for (let i = 0; i < this.size; i++) {
+                console.log(i + 1, this.todoList[i]);
             }
         }
         else {
             console.log(`You don't have any todo lists.\n`);
         }
     }
-};
-
-const todo: TodoItem = new TodoItem();
-
-let haveConversation: boolean = true;
-
-while (haveConversation){
+}
+;
+const todo = new TodoItem();
+let haveConversation = true;
+while (haveConversation) {
     console.log(`Welcome to your todo app!\n`);
     console.log(`Choose what you want to do: `);
     console.log(`1 to display your todo lists`);
     console.log(`2 to add new todo`);
     console.log(`3 to remove a todo`);
     console.log(`4 to end this conversation`);
-
     let userInput = prompt("Insert your choice here");
-    
-    if ( userInput == null || +userInput < 1 || +userInput > 4){
+    if (userInput == null || +userInput < 1 || +userInput > 4) {
         console.log("You have to choose one of the numbers from the given alternatives.");
     }
-    else{
-        switch(+userInput){
+    else {
+        switch (+userInput) {
             case 1:
                 todo.display();
                 break;
             case 2:
-                let text: string | null = prompt("Please provide your todo.");
+                let text = prompt("Please provide your todo.");
                 if (text != null)
                     todo.add(text);
                 break;
@@ -77,8 +63,6 @@ while (haveConversation){
             case 4:
                 haveConversation = false;
                 break;
-            
         }
     }
-
 }
